61bb506c2af1dba8111fa7ff8866786e03e3b386
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-09-23 19:17:43 -0300

    object js

---
+++ js/dados.js
@@ -0,0 +1,68 @@
+// Base de dados
+
+// Armazenamento dos personagens em uma lista de objetos
+
+let dados = [
+    // Davi
+    {
+        titulo: "Davi",
+        imagem: null,
+        descricao: "Davi foi um rei de Israel, conhecido por sua coragem, fé em Deus e talento musical. Ungido por Samuel ainda jovem, ele derrotou o gigante Golias e reinou sobre as tribos de Israel.",
+        epoca: null,
+        local: null,
+        livros: "1 Samuel, 2 Samuel, 1 Reis, Salmos",
+        versiculos: null,
+        // Filhos, parentes, vídeo(s) relativo, lições, histórias marcantes, significado do nome () //titulo -> nome
+        // papel: null,
+        // Imagem ->   
+    },
+
+    // Moisés
+    {
+
+    },
+
+    // Jonas
+    {
+
+    },
+
+    // Padrão
+    {
+        nome: "",
+        significadoNome: "",
+        imagem: "",
+        descricao: "",
+        epoca: "",
+        localizacao: "",
+        papel: "",
+        livrosBiblicos: [],
+        versiculo: ""
+    }
+]
+
+// Padrão
+/*{
+    nome: "",
+    imagem: "",
+    descricao: "",
+    livrosBiblicos: [],
+    versiculo: "",
+    epoca: "",
+    localizacao: "",
+    relacoes: [],
+    profissao: "",
+    caracteristicas: [],
+    licoes: [],
+    citacaoDireta: "",
+    simbolos: [],
+    representacoesArtisticas: [],
+    significadoNome: ""
+}
+
+const personagemA = {
+    //...
+}
+
+personagens = [personagemA, personagemB, personagemC]
+*/
